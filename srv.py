6920a85a08e6ef7039700d1f1aa7f4dc77ee7124
Lisa <liza.sosiska95@gmail.com>
2020-08-05 18:59:44 +0300

    srv.py

--- srv.py
+++ srv.py
@@ -1,24 +1,52 @@
 import os
 import socketserver
+
+
 from http.server import SimpleHTTPRequestHandler
-PORT = int(os.getenv("PORT", 8000))
-print(PORT)
-class MyHandler(SimpleHTTPRequestHandler):
+
+class MyHttp(SimpleHTTPRequestHandler):
     def do_GET(self):
-        content = """
+        path = self.build_path()
+
+        if path == "/":
+            self.handle_root()
+        elif path == "/hello/":
+            self.handle_hello()
+        else:
+            self.handle_404()
+
+    def handle_root(self):
+        return super().do_GET()
+
+    def handle_hello(self):
+        content = f"""
         <html>
-        <head><title>xxx</title></head>
-        <body>Hello World!</body>
+        <head><title>Hello Page</title></head>
+        <body>
+        <h1>Hello world!</h1>
+        <p>path: {self.path}</p>
+        </body>
         </html>
         """
 
-        self.send_response(200)
-        self.send_header("Content-type","text/html")
-        self.send_header("Content-lenght", str(len(content)))
-        self.end_headers()
-        self.wfile.write(content.encode())
+        self.respond(content)
 
-if __name__ == "__main__":
-    with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
-        print("it" + " works")
-        httpd.serve_forever(poll_interval=1)
+    def handle_404(self):
+        msg = """NOT FOUND!!!!!!!!"""
+
+        self.respond(msg, code=404, content_type="text/plain")
+
+    def respond(self, message, code=200, content_type="text/html"):
+        self.send_response(code)
+        self.send_header("Content-type", content_type)
+        self.send_header("Content-length", str(len(message)))
+        self.end_headers()
+        self.wfile.write(message.encode())
+
+    def build_path(self) -> str:
+        result = self.path
+
+        if result[-1] != "/":
+            result = f"{result}/"
+
+        return result
